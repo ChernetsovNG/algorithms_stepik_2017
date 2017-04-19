87d308c47d9a3553bc45f94e618c8b6c4f140166
Nikita Chernetsov <n.chernetsov86@gmail.com>
2017-04-19 15:43:38 +0300

    Week 1 - Problem 3 - Задача прошла в таком (достаточно уродливом) виде

--- src/Week1/problem_3/Main.java
+++ src/Week1/problem_3/Main.java
@@ -9,6 +9,7 @@ class Packet {
     int arrival;    //время прибытия
     int duration;   //время обработки
     int timeStart;  //момент начала обработки (-1 - если пакет отброшен)
+    boolean isProcessed;  //был ли пакет обработан? (добавлен в буфер или отброшен)
 
     public Packet() {
     }
@@ -38,6 +39,7 @@ public class Main {
             packets[i].arrival = Integer.parseInt(s2Array[0]);
             packets[i].duration = Integer.parseInt(s2Array[1]);
             packets[i].timeStart = -1;
+            packets[i].isProcessed = false;
         }
 
         Queue<Packet> buffer = new LinkedList<>();
@@ -46,38 +48,15 @@ public class Main {
         int numHandlingPacket = 0; //номер рассматриваемого пакета
         Packet RunningPacket = new Packet();  //пакет, с которым работает компьютер
         boolean isRun = false;     //работает ли (true) или простаивает компьютер
+        int countCompProcessedPackets = 0;  //кол-во обработанных компьютером пакетов
 
-        while (numHandlingPacket < n) {
-
-            //1. добавляем пакеты в буфер
-            while (packets[numHandlingPacket].arrival == time) {
-                if (buffer.size() < size) {
-                    //если буфер пустой, и приходит пакет с нулевой длительностью обработки, и при этом компьютер свободен,
-                    //то его сразу же обрабатываем
-                    if ((buffer.size() == 0) && (packets[numHandlingPacket].duration == 0) && (!isRun)) {
-                        packets[numHandlingPacket].timeStart = time;
-                        numHandlingPacket++;
-                        if (numHandlingPacket < n)
-                            continue;
-                        else
-                            break;
-                    }
-                    buffer.offer(packets[numHandlingPacket]);
-                } else {
-                    packets[numHandlingPacket].timeStart = -1;  //пакет не влезает в буфер - отбрасываем
-                }
-                numHandlingPacket++;
-                if (numHandlingPacket < n)
-                    continue;
-                else
-                    break;
-            }
-
+        while ((numHandlingPacket < n) || (countCompProcessedPackets < n)) {
             //2. Те пакеты, что компьютер обработал, больше не рассматриваем
             if (isRun) {
                 if (RunningPacket.timeStart + RunningPacket.duration == time) {
                     //закончили обработку пакета, и готовы взять следующий из буфера
                     buffer.poll();  //удаляем пакет из буфера
+                    countCompProcessedPackets++;
                     isRun = false;
                 }
             }
@@ -88,6 +67,70 @@ public class Main {
                     RunningPacket = buffer.peek();  //берём пакет из буфера, но пока не удаляем
                     RunningPacket.timeStart = time;
                     isRun = true;
+                    if (RunningPacket.duration == 0) {  //если продолжительность нулевая, то сразу обрабатываем пакет
+                        buffer.poll();
+                        countCompProcessedPackets++;
+                        isRun = false;
+                    }
+                } else {
+                    isRun = false;  //ждём появления пакета в буфере
+                    RunningPacket = null;
+                    break;
+                }
+            }
+
+            //1. добавляем пакеты в буфер
+            if (numHandlingPacket < n) {
+                while ((packets[numHandlingPacket].arrival == time) && (packets[numHandlingPacket].isProcessed == false)) {
+                    if (buffer.size() < size) {
+                        //если буфер пустой, и приходит пакет с нулевой длительностью обработки, и при этом компьютер свободен,
+                        //то его сразу же обрабатываем
+                        if ((buffer.size() == 0) && (packets[numHandlingPacket].duration == 0) && (!isRun)) {
+                            packets[numHandlingPacket].timeStart = time;
+                            packets[numHandlingPacket].isProcessed = true;
+                            numHandlingPacket++;
+                            countCompProcessedPackets++;
+                            if (numHandlingPacket < n)
+                                continue;
+                            else
+                                break;
+                        }
+                        buffer.offer(packets[numHandlingPacket]);
+                        packets[numHandlingPacket].isProcessed = true;
+                    } else {
+                        packets[numHandlingPacket].timeStart = -1;  //пакет не влезает в буфер - отбрасываем
+                        countCompProcessedPackets++;
+                        packets[numHandlingPacket].isProcessed = true;
+                    }
+                    numHandlingPacket++;
+                    if (numHandlingPacket < n)
+                        continue;
+                    else
+                        break;
+                }
+            }
+
+            //2. Те пакеты, что компьютер обработал, больше не рассматриваем
+            if (isRun) {
+                if (RunningPacket.timeStart + RunningPacket.duration == time) {
+                    //закончили обработку пакета, и готовы взять следующий из буфера
+                    buffer.poll();  //удаляем пакет из буфера
+                    countCompProcessedPackets++;
+                    isRun = false;
+                }
+            }
+
+            //3. Работа компьютера
+            while (!isRun) {
+                if (buffer.size() > 0) {  //если буфер не пустой
+                    RunningPacket = buffer.peek();  //берём пакет из буфера, но пока не удаляем
+                    RunningPacket.timeStart = time;
+                    isRun = true;
+                    if (RunningPacket.duration == 0) {  //если продолжительность нулевая, то сразу обрабатываем пакет
+                        buffer.poll();
+                        countCompProcessedPackets++;
+                        isRun = false;
+                    }
                 } else {
                     isRun = false;  //ждём появления пакета в буфере
                     RunningPacket = null;
