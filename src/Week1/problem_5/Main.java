f1261bb0c9a0cc20de9f608a43a32026fc62f13b
ChernetsovNG <n.chernetsov86@gmail.com>
2017-04-19 11:03:20 +0300

    Week 1 - Problem 5 - Добавил два класса: 1) стек с поддержкой максимума; 2) очередь на основе двух таких стеков

--- src/Week1/problem_5/Main.java
+++ src/Week1/problem_5/Main.java
@@ -1,6 +1,73 @@
 package Week1.problem_5;
 
 import java.util.Scanner;
+import java.util.Stack;
+
+class StackMax {
+    private Stack<Integer> stack;     //стек значений
+    private Stack<Integer> stackMax;  //стек максимумов
+
+    public StackMax() {
+        stack = new Stack<>();
+        stackMax = new Stack<>();
+    }
+
+    public void push(Integer v) {
+        stack.push(v);
+        if (stackMax.size() > 0) {
+            if (v >= stackMax.peek()) {
+                stackMax.push(v);
+            } else {
+                stackMax.push(stackMax.peek());
+            }
+        } else {
+            stackMax.push(v);
+        }
+    }
+
+    public Integer pop() {
+        stackMax.pop();
+        return stack.pop();
+    }
+
+    public Integer max() {
+        return stackMax.peek();
+    }
+
+    public boolean isEmpty() {
+        return (stack.size() == 0);
+    }
+
+    public int size() {
+        return stack.size();
+    }
+}
+
+class QueueStackMax {  //очередь на основе стеков с поддержкой максимума
+    private StackMax stackLeft;
+    private StackMax stackRight;
+
+    public QueueStackMax() {
+        stackLeft = new StackMax();
+        stackRight = new StackMax();
+    }
+
+    public void pushBack(Integer i) {
+
+    }
+
+    public Integer popFront() {
+        return 0;
+    }
+
+    public boolean isEmpty() {
+        return false;
+    }
+
+    public int size() {
+        return 0;
+    }
+}
 
 public class Main {
 
