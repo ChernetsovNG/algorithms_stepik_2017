01fb517f24d9fafbfed5f6ca051ff746f9ed789a
ChernetsovNG <n.chernetsov86@gmail.com>
2017-04-29 20:54:56 +0300

    Week 2 - Problem 3 - Немного оптимизировал, 46 тест не проходит

--- src/Week2/problem_3/Main.java
+++ src/Week2/problem_3/Main.java
@@ -1,7 +1,5 @@
 package Week2.problem_3;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 class DisjointSet {
@@ -9,7 +7,6 @@ class DisjointSet {
     public int[] rank;
     public int[] tablesSize;
     public int maxTableSize = 0;
-    public int maxTableId = 0;
 
     public DisjointSet(int size, int[] tablesSize) {
         parent = new int[size];
@@ -21,7 +18,6 @@ class DisjointSet {
             rank[i] = 0;
             if (tablesSize[i] > maxTableSize) {
                 maxTableSize = tablesSize[i];
-                maxTableId = i;
             }
         }
     }
@@ -44,25 +40,30 @@ class DisjointSet {
         if (rank[i_id] > rank[j_id]) {
             parent[j_id] = i_id;
 
-            tablesSize[i_id] = tablesSize[i_id] + tablesSize[j_id];
-            tablesSize[j_id] = 0;
+            if (tablesSize[j_id] > 0) {
 
-            if (tablesSize[i_id] > maxTableSize) {
-                maxTableSize = tablesSize[i_id];
+                tablesSize[i_id] += tablesSize[j_id];
+                tablesSize[j_id] = 0;
+
+                if (tablesSize[i_id] > maxTableSize) {
+                    maxTableSize = tablesSize[i_id];
+                }
             }
         } else {
             parent[i_id] = j_id;
 
-            tablesSize[j_id] = tablesSize[i_id] + tablesSize[j_id];
-            tablesSize[i_id] = 0;
+            if (tablesSize[i_id] > 0) {
 
-            if (tablesSize[j_id] > maxTableSize) {
-                maxTableSize = tablesSize[j_id];
+                tablesSize[j_id] += tablesSize[i_id];
+                tablesSize[i_id] = 0;
+
+                if (tablesSize[j_id] > maxTableSize) {
+                    maxTableSize = tablesSize[j_id];
+                }
             }
 
             if (rank[i_id] == rank[j_id]) {
-                int k = rank[j_id];
-                rank[j_id] = k+1;
+                rank[j_id] += 1;
             }
         }
     }
@@ -88,22 +89,21 @@ public class Main {
             r[i] = Integer.parseInt(s2Arr[i]);
         }
 
-        int[] dest = new int[m];
-        int[] src = new int[m];
-
-        for (int i = 0; i < m; i++) {
-            String s3 = sc.nextLine();
-            String[] s3Arr = s3.split(" ");
-
-            dest[i] = Integer.parseInt(s3Arr[0])-1;
-            src[i] = Integer.parseInt(s3Arr[1])-1;
-        }
-
-        //Решение
         DisjointSet set = new DisjointSet(n, r);
 
+        int dest;
+        int src;
+        String s3;
+        String[] s3Arr;
+
         for (int i = 0; i < m; i++) {
-            set.union(dest[i], src[i]);
+            s3 = sc.nextLine();
+            s3Arr = s3.split(" ");
+
+            dest = Integer.parseInt(s3Arr[0])-1;
+            src = Integer.parseInt(s3Arr[1])-1;
+
+            set.union(dest, src);
             System.out.println(set.maxTableSize);
         }
 
