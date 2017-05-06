42b363ea5d8a7eb50d7da3c9947a2b80976add15
ChernetsovNG <n.chernetsov86@gmail.com>
2017-05-06 19:15:47 +0300

    Week 3 - Problem 3 - Сделал вычисление массива хеш-кодов окон (по алгоритму Рабина-Карпа)

--- src/Week3/problem_3/Main.java
+++ src/Week3/problem_3/Main.java
@@ -1,30 +1,29 @@
 package Week3.problem_3;
 
+import java.util.Scanner;
+
+//Реализация алгоритма Рабина-Карпа
 public class Main {
     private static final int p = 1_000_000_007;  //простое число
     private static final int x = 263;  //основание хеш-фукнции
-    private static int m = 5;
 
-    public static long pow(int base, int exp, int mod) {
-        long a = base;
-        long b = exp;
-        long n = mod;
-
+    //вычисляет (base^exp) % mod
+    private static long pow(int base, int exp, int mod) {
         long x = 1;
-        long y = a;
+        long y = base;
 
-        while (b > 0) {
-            if(b % 2 == 1) {
-                x = ((x % n) * (y % n)) % n;
+        while (exp > 0) {
+            if(exp % 2 == 1) {
+                x = ((x % mod) * (y % mod)) % mod;
             }
-            y = ((y % n) * (y % n)) % n;
-            b /= 2;
+            y = ((y % mod) * (y % mod)) % mod;
+            exp /= 2;
         }
-        return x % n;
+        return x % mod;
     }
 
     //вычисление хеш-функции
-    public static int hash(String s) {
+    private static int hash(String s) {
         char[] chars = s.toCharArray();
         long sumP = 0;
 
@@ -35,14 +34,47 @@ public class Main {
 
             sumP += add % p;
         }
-        sumP = sumP % p;
-        sumP = sumP % m;
-
-        return (int) sumP;
+        return (int) (sumP % p);
     }
 
 
     public static void main(String[] args) {
-        System.out.println(hash("HellO"));
+        Scanner sc = new Scanner(System.in);
+
+        String pattern = sc.nextLine();
+        String text = sc.nextLine();
+
+        int P = pattern.length();
+        int T = text.length();
+
+        long[] x_pow = new long[P];
+
+        //предвычисление массива степеней полинома
+        for (int i = 0; i < P; i++) {
+            x_pow[i] = pow(x, i, p);
+        }
+        int hashPattern = hash(pattern);
+
+        int[] hashWindow = new int[T - P + 1];  //hashCode всех "окон" (слева направо!)
+
+        String window = text.substring(T - P, T);
+
+        hashWindow[T - P] = hash(window);
+
+        //Идём по строке справа-налево и вычисляем hash
+        for (int i = T - 2; i >= P - 1; i--) {
+            int charIP = (int) text.charAt(i + 1);
+            int charI = (int) text.charAt(i - P + 1);
+
+            long hash_i = hashWindow[i - P + 2];
+            hash_i -= ((charIP % p) * x_pow[P - 1]) % p;
+            hash_i = ((hash_i % p) * (x % p)) % p;
+            hash_i = ((hash_i % p) + (charI % p)) % p;
+
+            hashWindow[i - P + 1] = (int) hash_i;
+        }
+
+
+
     }
 }
